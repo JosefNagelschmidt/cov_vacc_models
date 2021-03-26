8c42c158ecb96c273032e981767f6e0689b98233
Josef Nagelschmidt <j.nagelschmidt@uni-bonn.de>
2021-03-26 01:10:12 +0100

    The following changes are made:
    - Some output files are added to the ./src/final folder, in order to enable the inspection of results without prior replication of the full project
    - new jupyter notebooks are added to the ./src/final folder, to visualize the results
    - sandboxing is updated, but will change in the future (possible removal of legacy files)
    - the references are updated

--- src/sandbox/lasso_estimator_glmnet_comparison.R
+++ src/sandbox/lasso_estimator_glmnet_comparison.R
@@ -8,3 +8,26 @@ scaled.dat = scale(df)
 
 fit <- glmnet(df[,-1], df[,1])
 coef(fit, s = 0.01, exact = TRUE, x = df[,-1] , y= df[,1])
+
+
+library("clusterGeneration")
+
+data <- genRandomClust(numClust = 10,sepVal = 0.15,numNonNoisy = 90,numNoisy = 10,numReplicate = 1,fileName = "chk1")
+cluster_data = data$datList$chk1_1
+
+genPositiveDefMat$datList
+
+cov_mat = genPositiveDefMat(dim = 100,covMethod = "eigen")
+cov = cov_mat$Sigma
+a = diag(cov)
+diag(cov) <- a / 6
+cor = cov2cor(cov)
+
+library("MASS")
+highCor<-matrix(seq(from=0.9, to= 0.0001, length.out = 100),100,100)
+
+
+n <- 100
+A <- matrix(runif(n^2)*4-1, ncol=n)
+cov <- t(A) %*% A
+
